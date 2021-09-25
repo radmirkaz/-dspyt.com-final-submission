9bf6c28ad643c7600b2ed904dcf17fb3fe1fe1f5
RadmirZ <69034231+RadmirZ@users.noreply.github.com>
2021-09-25 20:15:21 +0300

    Delete raifhack-ds directory

--- raifhack-ds/utils.py
+++
@@ -1,8 +0,0 @@
-from enum  import IntEnum
-
-UNKNOWN_VALUE = 'missing'
-
-class PriceTypeEnum(IntEnum):
-
-    OFFER_PRICE = 0 # цена из объявления
-    MANUAL_PRICE = 1 # цена, полученная путем ручной оценки
